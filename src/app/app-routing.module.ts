624e533f39c73d003e8a5f573bb420f71b30631d
Himanshu Parihar <71928859+iHimanshuParihar@users.noreply.github.com>
2022-12-12 23:42:07 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllusersComponent } from './components/allusers/allusers.component';
+import { ToppersComponent } from './components/toppers/toppers.component';
+import { UsersComponent } from './components/users/users.component';
+import { WinnersComponent } from './components/winners/winners.component';
 
 const routes: Routes = [
+  {path:'**',component:AllusersComponent},
   {path:'all',component:AllusersComponent},
-  {path:'topper',component:AllusersComponent},
-  {path:'user',component:AllusersComponent},
-  {path:'winners',component:AllusersComponent},
+  {path:'topper',component:ToppersComponent},
+  {path:'user',component:UsersComponent},
+  {path:'winners',component:WinnersComponent},
 ];
 
 @NgModule({
